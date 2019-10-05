e5616d74c6a9eb98b88af81f59b8f9fdb42b0e7f
John Doyle <johndoyle@gmail.com>
2019-10-05 14:30:38 -0400

    edited for heroku environment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let path = require("path")
 let app = express();
-var PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,4 +12,6 @@ app.use(express.static("app/public"));
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
-console.log('Server is listening on port ' + port);
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
