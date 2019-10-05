ab27f0eec063441184caa796ded376fe027b97eb
John Doyle <johndoyle@gmail.com>
2019-10-05 14:17:01 -0400

    "changed port for production"

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let path = require("path")
 let app = express();
-let port = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
